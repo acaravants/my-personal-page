a8438f408122e56c6f62c0f857b218685fff87a6
alejo <adac4r@gmail.com>
2018-06-03 00:55:05 -0500

    feat added webcomponents vanilla test

---
+++ example-button.js
@@ -0,0 +1,35 @@
+class SellButton extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    let shadowRoot = this.attachShadow({mode: 'open'});
+    shadowRoot.innerHTML = `
+    <style>
+      :host {
+        --orange: #e67e22;
+        --space: 1.5em;
+      }
+      .btn-container {
+        border: 2px dashed var(--orange);
+        padding: var(--space);
+        text-align: center;
+      }
+      .btn {
+        background-color: var(--orange);
+        border: 0;
+        border-radius: 5px;
+        color: white;
+        padding: var(--space);
+        text-transform: uppercase;
+      }
+    </style>
+    <div class="btn-container">
+      <button class="btn">Comprar ahora</button>
+    </div>`;
+
+  }
+}
+
+window.customElements.define('sell-button', SellButton);
